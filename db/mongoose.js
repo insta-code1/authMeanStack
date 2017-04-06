bb69cc5d90a374c9cab18b337e3945ae6ec0fcea
insta-code1 <test@insta-code.io>
2017-04-06 17:39:05 +0100

    Connecting to mongodb

---
+++ db/mongoose.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/authapp');
+mongoose.connection
+  .once('open', () => console.log('Connected to Mongodb'))
+  .on('error', error => console.log(`Mongodb Connection Error: ${error}`));
