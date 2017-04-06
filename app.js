969c606125785cd0fd617f7861e54973164f9918
insta-code1 <test@insta-code.io>
2017-04-06 15:34:55 +0100

    Add app.js and user routes

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+const routes = require('./routes/users');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routes);
+
+const server = app.listen(port, () => {
+  const host =  'localhost';
+  console.log(`App is listening at http://${host}:${port}`);
+});
