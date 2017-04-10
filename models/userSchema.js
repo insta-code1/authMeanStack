23f117c8482fc18372da205e16984cee9260f5e2
insta-code1 <test@insta-code.io>
2017-04-10 15:27:47 +0100

    encrypting the users password with bcryptjs

--- models/userSchema.js
+++ models/userSchema.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 const validator = require('validator');
 const uniqueValidator = require('mongoose-unique-validator');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 
 const userSchema = new Schema({
@@ -33,6 +34,25 @@ const userSchema = new Schema({
 });
 
 userSchema.plugin(uniqueValidator);
+
+
+userSchema.pre('save', function(next) {
+  let user = this;
+  if(user.isModified('password')) {
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    })
+  } else {
+    next();
+  }
+});
+
+
+
 
 
 userSchema.methods.confirmUser = function () {
